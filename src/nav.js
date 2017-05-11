00eb3c062a3f7c7646da720c9ee0603d899b3f8f
sgeofrank <sgeofrank@gmail.com>
2017-05-11 16:38:07 -0500

    syntax

--- src/nav.js
+++ src/nav.js
@@ -1,3 +1,4 @@
 export default function navToggle(x) {
-    x.classList.toggle("change");
-    document.getElementById("navpane").classList.toggle("display");}
+	x.classList.toggle("change")
+	document.getElementById("navpane").classList.toggle("display")
+}
