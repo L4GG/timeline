c5479f9a317adedd1c8737312de8582609c5743b
sgeofrank <sgeofrank@gmail.com>
2017-05-11 19:01:03 -0500

    whitespace and files

--- src/nav.js
+++ src/nav.js
@@ -1,4 +1,4 @@
 export default function navToggle(x) {
 	x.classList.toggle("change")
-	document.getElementById("navpane").classList.toggle("display")
+	document.getElementById("navpane").classList.toggle("display") // eslint-disable-line no-undef
 }
