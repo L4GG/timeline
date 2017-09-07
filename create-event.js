35c32d22317d7682b61cff6cdcee6e377dd97f5e
Andrew Hyndman <ajhyndman@hotmail.com>
2017-09-07 20:16:02 +1000

    Throw instead of root level return in node script

--- create-event.js
+++ create-event.js
@@ -7,15 +7,14 @@ const EventTemplate = require('./src/utils/EventTemplate')
 const args = JSON.parse(process.env.npm_config_argv)
 
 if (args.remain.length < 2) {
-	return console.error('An event name and date are required. For example: \nnpm run create "Event Name" "2017-02-28 19:30"')
+	throw new Error('An event name and date are required. For example: \nnpm run create "Event Name" "2017-02-28 19:30"')
 }
 
 const [eventName, eventDateString] = args.remain
 const eventDate = moment.utc(eventDateString, 'YYYY-MM-DD HH:mm')
 
 if (!eventDate.isValid()) {
-	console.error(`"${eventDateString}" is an invalid date. Please pass a date in the format "YYYY-MM-DD HH:mm".`)
-	return
+	throw new Error(`"${eventDateString}" is an invalid date. Please pass a date in the format "YYYY-MM-DD HH:mm".`)
 }
 const eventPath = path.join(process.cwd(), 'src', 'events', String(eventDate.year()), Case.kebab(eventName))
 mkdirp(eventPath, err => {
