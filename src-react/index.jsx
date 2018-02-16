fe07438b71cd0b1fa245280c21d5a8e9c0044b8d
Andrew Hyndman <ajhyndman@hotmail.com>
2018-02-16 21:07:23 +1100

    WIP

---
+++ src-react/index.jsx
@@ -0,0 +1,8 @@
+// @flow
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Text } from 'react-primitives';
+
+const App = () => <Text>Hello World!</Text>;
+
+export default App;
